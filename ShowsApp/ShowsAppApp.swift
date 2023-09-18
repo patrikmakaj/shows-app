e1234584c749151d715681c39ff8222350476b20
Patrik Makaj <patrikmakaj01@gmail.com>
2023-09-18 10:41:57 +0200

    Initial Commit

---
+++ ShowsApp/ShowsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowsAppApp.swift
+//  ShowsApp
+//
+//  Created by Patrik Makaj on 18.09.2023..
+//
+
+import SwiftUI
+
+@main
+struct ShowsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
